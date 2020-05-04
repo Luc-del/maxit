15fdb41f7a4255f8b53f8e3b311cdbe3aeb1da10
Lucdel78 <34277178+Lucdel78@users.noreply.github.com>
2020-05-04 09:12:18 +0200

    gridview adapter

--- app/src/main/java/com/example/maxit/Game.java
+++ app/src/main/java/com/example/maxit/Game.java
@@ -8,10 +8,13 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.GridView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class Game extends AppCompatActivity {
 
@@ -19,6 +22,10 @@ public class Game extends AppCompatActivity {
 
     int Nx = 3;
     int Ny = 4;
+
+    GridView gridview;
+    GridViewAdapter gridviewAdapter;
+    ArrayList<Cell> data = new ArrayList<Cell>();
 
     private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
         @Override
@@ -35,37 +42,45 @@ public class Game extends AppCompatActivity {
 
         setContentView(R.layout.game);
 
-        instanciateGrid();
-
-        Play();
-
-
-    }
-
-    public void instanciateGrid() {
-        Grid.setSize(Nx,Ny);
-    }
-
-    public void Play() {
+        initView(); // Initialize the GUI Components
+        fillData(); // Insert The Data
+        setDataAdapter(); // Set the Data Adapter
 
     }
 
-    public boolean onTouchEvent(@NonNull MotionEvent event) {
-        float x = event.getX();
-        float y = event.getY();
 
-        
-        return true;
+    // Initialize the GUI Components
+    private void initView()
+    {
+        gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setNumColumns(Nx);
+//        gridview.setOnItemClickListener(this);
     }
+
+    // Insert The Data
+    private void fillData()
+    {
+        for(int i = 0; i < Ny; i++)
+        {
+            for(int j = 0; j < Nx; j++) {
+                int k = i*Nx+j;
+                data.add(new Cell(k));
+            }
+        }
+    }
+
+    // Set the Data Adapter
+    private void setDataAdapter()
+    {
+        gridviewAdapter = new GridViewAdapter(getApplicationContext(), R.layout.game, data);
+        gridview.setAdapter(gridviewAdapter);
+    }
+
+//    @Override
+//    public void onItemClick(final AdapterView<?> arg0, final View view, final int position, final long id)
+//    {
+//        String message = "Clicked : " + data.get(position).getTitle();
+//        Toast.makeText(getApplicationContext(), message , Toast.LENGTH_SHORT).show();
+//    }
 
 }
-
-//    public boolean onTouchEvent(MotionEvent event) {
-//        // MotionEvent object holds X-Y values
-//        if(event.getAction() == MotionEvent.ACTION_DOWN) {
-//            String text = "You click at x = " + event.getX() + " and y = " + event.getY();
-//            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
-//        }
-//
-//        return super.onTouchEvent(event);
-//    }
